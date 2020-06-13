d6755c9aabe8521ffaf33cb8f8c6fce0e3a93990
Raj Shah <raj.shah@robosoftin.com>
2020-06-13 17:44:18 +0530

    build: added es6 support and added badge's

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # starwars-names
+
+[![travis build](https://img.shields.io/travis/rajshah-robosoft/starwars-names.svg?style=flat-square)](https://travis-ci.org/github/rajshah-robosoft/starwars-names)
+[![codecov coverage](https://img.shields.io/codecov/c/github/rajshah-robosoft/starwars-names.svg?style=flat-square)](https://codecov.io/gh/rajshah-robosoft/starwars-names)
+[![version](https://img.shields.io/npm/v/raj-shah-starwars-names.svg?style=flat-square)](https://www.npmjs.com/raj-shah-starwars-names)
+[![downloads](https://img.shields.io/npm/dw/raj-shah-starwars-names)](https://www.npmjs.com/raj-shah-starwars-names)
+[![MIT License](https://img.shields.io/npm/l/raj-shah-starwars-names)](https://www.npmjs.com/raj-shah-starwars-names)
+![]()
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
-var expect = require('chai').expect;
-var starWars = require('./index');
+import { expect } from 'chai';
+import { getAllNames, getRandomName, randomLists, isNameAvailable, getListFromSearchItem } from './index';
 
 describe('raj-shah-starwars-names', function () {
   describe('getAllNames', function () {
     it('should be array of string', function () {
-      expect(starWars.getAllNames).to.satisfy(isArrayOfStrings);
+      expect(getAllNames).to.satisfy(isArrayOfStrings);
 
       function isArrayOfStrings(array) {
         return array.every(function (item) {
@@ -14,33 +14,33 @@ describe('raj-shah-starwars-names', function () {
     })
 
     it('should contain `Luke Skywalker`', function () {
-      expect(starWars.getAllNames).to.include('Luke Skywalker');
+      expect(getAllNames).to.include('Luke Skywalker');
     })
   })
 
   describe('random', function () {
     it('should return a random item from the starWars.getAllNames', function () {
-      var randomItem = starWars.getRandomName();
-      expect(starWars.getAllNames).to.include(randomItem);
+      var randomItem = getRandomName();
+      expect(getAllNames).to.include(randomItem);
     })
 
     it('should return an array of random items if passed a number', function () {
-      let randomItems = starWars.randomLists(3);
+      let randomItems = randomLists(3);
       randomItems.forEach(function (item) {
-        expect(starWars.getAllNames).to.include(item);
+        expect(getAllNames).to.include(item);
       })
     })
   })
 
   describe('find', function () {
     it('should return true or false', function () {
-      expect(starWars.isNameAvailable('Luke Skywalker')).to.equal(true);
+      expect(isNameAvailable('Luke Skywalker')).to.equal(true);
     })
   })
 
   describe('getListFromSearchItem', function () {
     it('should return array of names', function () {
-      expect(starWars.getListFromSearchItem('luke')).to.deep.equal([ 'Luke Skywalker' ]);
+      expect(getListFromSearchItem('luke')).to.deep.equal([ 'Luke Skywalker' ]);
     })
   })
 })
